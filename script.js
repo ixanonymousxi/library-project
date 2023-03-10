f09dd39a484c6aed06641fd521e63f4d52bf9c7a
Sarah <louise562@aol.com>
2023-03-10 07:43:17 -0600

    Added structual HTML and CSS.

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title,author,pageCount,readStatus) {
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
